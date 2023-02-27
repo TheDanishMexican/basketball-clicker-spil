efff41d875ab92b2f2dc6992b7919d83da853a38
TheDanishMexican <danieljchristjansen@gmail.com>
2023-02-27 16:37:40 +0100

    sd

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ let points = 0;
 let life = 3;
 
 function start() {
-  // document.querySelector("button").addEventListener("click", startGame);
-
-  // document.querySelector("button").addEventListener("click", displayNameWin);
-
-  // document.querySelector("button").addEventListener("click", displayNameLose);
-
   addPosition();
 
   addAnimation();
@@ -68,19 +62,99 @@ function addClick() {
   let fball = document.querySelector("#football_container");
   let sball = document.querySelector("#soccerball_container");
   let fbball = document.querySelector("#frenzy_basketball_container");
-  let bball1sprite = document.querySelector("#basketball_sprite");
 
-  bball1.classList.add("mousedown", bballClicked);
-  bball2.classList.add("mousedown", bballClicked);
-  bball3.classList.add("mousedown", bballClicked);
-  fbball.classList.add("mousedown", bballClicked);
+  bball1.addEventListener("mousedown", bballClicked);
+  bball2.addEventListener("mousedown", bballClicked);
+  bball3.addEventListener("mousedown", bballClicked);
+  fbball.addEventListener("mousedown", bballClicked);
+  vball.addEventListener("mousedown", ballClicked);
+  fball.addEventListener("mousedown", ballClicked);
+  sball.addEventListener("mousedown", ballClicked);
 }
 
 function bballClicked() {
-  console.log("bball clicked");
   let ball = this;
+
+  ball.removeEventListener("mousedown", bballClicked);
+
   ball.classList.add("paused");
   ball.querySelector("img").classList.add("zoom_out");
 
-  coin.querySelector("img").classList.add("zoom_out");
+  ball.addEventListener("animationend", bballGone);
+}
+
+function ballClicked() {
+  let ball = this;
+
+  ball.removeEventListener("mousedown", ballClicked);
+
+  ball.classList.add("paused");
+  ball.querySelector("img").classList.add("zoom_out");
+  ball.addEventListener("animationend", ballGone);
+}
+
+function ballGone() {
+  console.log("ball is now gone");
+  let ball = this;
+
+  ball.removeEventListener("animationend", ballGone);
+
+  ball.classList.remove("shooting");
+  ball.classList.remove("zoom_out");
+  ball.querySelector("img").classList.remove("paused");
+  ballRestart.call(this);
+  ball.addEventListener("mousedown", ballClicked);
+}
+
+function bballGone() {
+  console.log("ball is now gone");
+  let ball = this;
+
+  ball.removeEventListener("animationend", ballGone);
+
+  ball.classList.remove("shooting");
+  ball.classList.remove("zoom_out");
+  ball.querySelector("img").classList.remove("paused");
+  bballRestart.call(this);
+  ball.addEventListener("mousedown", bballClicked);
+}
+
+function ballRestart() {
+  let ball = this;
+  ball.classList.remove("shooting");
+  ball.offsetWidth;
+  ball.classList.add("shooting");
+
+  ball.classList.remove(
+    "position1",
+    "position2",
+    "position3",
+    "position4",
+    "position5",
+    "position6",
+    "position7"
+  );
+  let pos = Math.floor(Math.random() * 7) + 1;
+
+  ball.classList.add("position" + pos);
+}
+
+function bballRestart() {
+  let ball = this;
+  ball.classList.remove("shooting");
+  ball.offsetWidth;
+  ball.classList.add("shooting");
+
+  ball.classList.remove(
+    "position1",
+    "position2",
+    "position3",
+    "position4",
+    "position5",
+    "position6",
+    "position7"
+  );
+  let pos = Math.floor(Math.random() * 5) + 1;
+
+  ball.classList.add("position" + pos);
 }
